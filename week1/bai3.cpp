1e024834526bdc6973ec65889cba4fa1448ddf66
Ly Duc Tu <direct.ldt@gmail.com>
2024-03-17 11:18:59 +0700

    Add files via upload

---
+++ week1/bai3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a;
+	string b;
+	char c;
+	float d;
+	double e;
+	cin >> a >> b >> c >> d >> e;
+	cout << a << '\n' << b << '\n' << c << endl;
+	cout << setprecision(3) << fixed << d << endl;
+	cout << setprecision(9) << fixed << e << endl;
+	return 0;
+}
