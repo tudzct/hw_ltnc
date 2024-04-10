403206e04c0f01314d80b4cc50adfa5971d7c7db
Ly Duc Tu <direct.ldt@gmail.com>
2024-04-10 22:10:06 +0700

    Add files via upload

--- week4/bai6.cpp
+++ week4/bai6.cpp
@@ -1,131 +1,46 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+void trace(string *grid, int w, int Y, int x, bool &survived, int h, vector<vector<bool>> &visited){
+    if(!survived){
+        if(x < 0 || x == w || visited[Y][x]) return;
+        visited[Y][x] = 1;
 
-/*
- * Complete the 'getTotalX' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
+        if(Y == h - 1){
+            survived = 1;
+            return;
+        }
 
-int gcd(int a, int b){
-    if(b == 0) return a;
-    return gcd(b, a % b);
+        else{
+            //move right
+            if((x <= w - 2) && (grid[Y][x + 1] == 'E') && (grid[Y + 1][x + 1] != 'R') && !survived){
+                trace(grid, w, Y + 1, x + 1, survived, h, visited);
+            }
+            //move left
+            if(x >= 1 && grid[Y][x - 1] == 'E' && grid[Y + 1][x - 1] != 'R' && !survived){
+                trace(grid, w, Y + 1, x - 1, survived, h, visited);
+            }
+            //still stand
+            if(grid[Y + 1][x] != 'R' && !survived){
+                trace(grid, w, Y + 1, x, survived, h, visited);
+            }
+        }
+    }
 }
 
-int lcm(int a, int b){
-    return (a / gcd(a, b)) * b;
-}
+int main(){
+    int w, h;
+    cin >> w >> h;
+    string str[2000];
+    for(int i = 0; i < h; i++) cin >> str[i];
 
-int getTotalX(vector<int> a, vector<int> b) {
-    int gcd2 = b[0];
-    for(int i = 1; i < b.size(); i++){
-        gcd2 = gcd(gcd2, b[i]);
-    }
+    int x = str[0].find('Y');
+    bool survived = 0;
+    vector<vector<bool>> visited(h, vector<bool>(w, 0));
 
-    int lcm1 = a[0];
-    for(int i = 1; i < a.size(); i++) {
-        lcm1 = lcm(lcm1, a[i]);
-    }
-
-    int cnt = 0;
-    for(int i = lcm1; i <= gcd2; i += lcm1) {
-        if(gcd2 % i == 0) cnt++;
-    }
-
-    return cnt;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
-
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    int n = stoi(first_multiple_input[0]);
-
-    int m = stoi(first_multiple_input[1]);
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    string brr_temp_temp;
-    getline(cin, brr_temp_temp);
-
-    vector<string> brr_temp = split(rtrim(brr_temp_temp));
-
-    vector<int> brr(m);
-
-    for (int i = 0; i < m; i++) {
-        int brr_item = stoi(brr_temp[i]);
-
-        brr[i] = brr_item;
-    }
-
-    int total = getTotalX(arr, brr);
-
-    fout << total << "\n";
-
-    fout.close();
-
+    trace(str, w, 0, x, survived, h, visited);
+    cout << (survived ? "YES" : "NO");
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
